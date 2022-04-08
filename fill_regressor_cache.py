8b7d7e9c16bdf7bf8b48263f3e1e28a6bde0811c
denis <dmelexin@mail.ru>
2022-04-08 07:57:54 +0300

    more

--- fill_regressor_cache.py
+++ fill_regressor_cache.py
@@ -6,25 +6,49 @@ from AUTO_regressor import *
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 from pprint import pprint
+from tqdm import tqdm
 
 
-regressor1 = AUTO_regressor(
-    currency_symbol="BTCUSDT",
-    seq_len=20,
-    regressor=CatBoostRegressor(random_state=123, silent=True),
-    ohlcv_size="1h",
-    columns=["high", "low"],
-    regressor_train_size=1000,
-    version="baseline_seqlen_20"
-)
+def fill(
+    start_date,
+    end_date,
+    currency_symbol,
+    ohlcv_size
+):
+    regressor = AUTO_regressor(
+        currency_symbol=currency_symbol,
+        seq_len=20,
+        regressor=CatBoostRegressor(random_state=123, silent=True),
+        ohlcv_size=ohlcv_size,
+        columns=["high", "low"],
+        regressor_train_size=1000,
+        version="baseline_seqlen_20"
+    )
 
-data1 = get_data(
-    "2022-01-01",
-    "2022-04-07",
-    "BTCUSDT",
-    "1h"
-)
+    data = get_data(
+        start_date,
+        end_date,
+        currency_symbol,
+        ohlcv_size
+    )
 
-regressor1.fill_up_cache(
-    df=data1
-)
+    regressor.fill_up_cache(
+        df=data
+    )
+
+if __name__ == "__main__":
+    currency_symbols = ["BTCUSDT"]
+    ohlcv_sizes = ["1h", "12h", "1d"]
+    start_date = "2019-01-01"
+    end_date = "2022-04-07"
+    
+    for currency_symbol in tqdm(currency_symbols):
+        for ohlcv_size in tqdm(ohlcv_sizes):
+            print(f"Filling {currency_symbol} size {ohlcv_size}")
+            fill(
+                start_date=start_date,
+                end_date=end_date,
+                currency_symbol=currency_symbol,
+                ohlcv_size=ohlcv_size
+            )
+            print()
